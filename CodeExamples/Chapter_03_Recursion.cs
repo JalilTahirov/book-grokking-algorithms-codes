6b7b4f37168d2d2e22712401e7f7e1d33f87a7c3
Jalil Tahirov <jalil.tahirov@tenforce.com>
2022-02-22 11:37:06 +0400

    Chapter 3 Factorial added and tested to understand call stack

--- CodeExamples/Chapter_03_Recursion.cs
+++ CodeExamples/Chapter_03_Recursion.cs
@@ -64,7 +64,13 @@ namespace CodeExamples
             CountDown(number-1);
         }
 
-
+        public int factorial(int num)
+        {
+            if (num == 1)
+                return 1;
+            else
+                return num * factorial(num-1);
+        }
         
 
     }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,19 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using System;
-using System.Runtime.InteropServices;
-using CodeExamples;
-
+﻿using System;
 Console.WriteLine("Hello, World!");
+/////// Chapter 04 Quicksort
 
-// Chapter 03 Recursion
-var chapterCode = new Chapter_03_Recursion();
-var mainBox = chapterCode.OrgonizeBoxes();
-chapterCode.LookForKeyRecursively(mainBox);
+
+
+
+
+
+
+/////// Chapter 03 Recursion
+//var chapterCode = new Chapter_03_Recursion();
+//var mainBox = chapterCode.OrgonizeBoxes();
+//chapterCode.LookForKeyRecursively(mainBox);
+
 //chapterCode.LookForKey(mainBox);
-chapterCode.CountDown(12);
 
-
+//chapterCode.CountDown(12);
+//var result = chapterCode.factorial(5);
+//Console.WriteLine(result);
 
 //var result = Chapter_01_BinarySearch.BinarySearch(new int[] { 1, 2, 3, 4, 5, 6, 7 }, 4);
 //Console.WriteLine(result);
